c56cb1df4b245a621a9ff9bbb9108d95e0a0fb16
Yuki <x1e6yhek@icloud.com>
2022-12-19 17:38:31 +0300

    task_10

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого
+Console.Clear();
+
+
+Console.WriteLine("Enter a three-digit number: ");
+string? n = Console.ReadLine();
+
+Console.WriteLine(Convert.ToInt32(n[1].ToString()));
